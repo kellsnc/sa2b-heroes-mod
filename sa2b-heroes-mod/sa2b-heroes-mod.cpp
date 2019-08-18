c1aa8c1888e656e1304b7473cea943bc36f82705
Kell <kellspam@outlook.fr>
2019-08-18 20:53:55 +0200

    Initial commit

---
+++ sa2b-heroes-mod/sa2b-heroes-mod.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+
+extern "C"
+{
+	__declspec(dllexport) void Init(const char *path, const HelperFunctions &helperFunctions)
+	{
+		
+	}
+
+	__declspec(dllexport) ModInfo SA2ModInfo = { ModLoaderVer };
+}
