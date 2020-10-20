1eccabbcef757fe7d4bc51505f6bd9e098605666
Kell <kellspam@outlook.fr>
2020-10-20 23:14:29 +0200

    Add EF Fans, fix sounds

--- sa2b-heroes-mod/egg-fleet.cpp
+++ sa2b-heroes-mod/egg-fleet.cpp
@@ -6,11 +6,113 @@
 ModelInfo* EF_SKYMDLS;
 ModelInfo* EF_PROPPLR;
 ModelInfo* EF_EHELICE;
+ModelInfo* EF_EBIGFAN;
+ModelInfo* EF_EBIGFANCOL;
 
 NJS_TEXLIST_ eggfleet_texlist = { arrayptrandlength(eggfleet_texname) };
 
-void __cdecl EFBigFans(ObjectMaster* obj) {
+bool Fans_IsSpecificPlayerInCylinder(EntityData1* entity, NJS_VECTOR* center, float radius, float height);
 
+void __cdecl EFBigFan_Display(ObjectMaster* obj) {
+	EntityData1* data = obj->Data1.Entity;
+	NJS_OBJECT* model = (NJS_OBJECT*)obj->field_4C;
+
+	njSetTexlist(CurrentLevelTexList);
+	njPushMatrix(0);
+	njTranslateEx(&data->Position);
+	njRotateY_(data->Rotation.y);
+	DrawSA2BModel(model->sa2bmodel);
+
+	njPopMatrix(0);
+	njRotateY_(data->Scale.z);
+	DrawSA2BModel(model->child->sa2bmodel);
+	njPopMatrix(1);
+
+	DrawSA2BModel(model->child->child->sa2bmodel);
+	njPopMatrix(1u);
+}
+
+void EFBigFan_DisplayAlpha(ObjectMaster* obj) {
+	EntityData1* data = obj->Data1.Entity;
+	NJS_OBJECT* model = (NJS_OBJECT*)obj->field_4C;
+
+	njSetTexlist(CurrentLevelTexList);
+	njPushMatrix(0);
+	njTranslateEx(&data->Position);
+	njRotateY_(data->Rotation.y);
+	DrawSA2BModel(model->child->child->sa2bmodel);
+	njPopMatrix(1u);
+}
+
+void __cdecl EFBigFan_Main(ObjectMaster* obj) {
+	if (ClipSetObject(obj)) {
+		EntityData1* data = obj->Data1.Entity;
+
+		if (MainCharObj1[0]) {
+			if (Fans_IsSpecificPlayerInCylinder(MainCharObj1[0], &data->Position, 130.0f, data->Scale.x)) {
+				data->Index = 20;
+			}
+
+			if (data->Index != 0) {
+				MainCharObj2[0]->Speed.y = 0.25f;
+				MainCharObj1[0]->Status |= 0x1000;
+				MainCharObj1[0]->NextAction = 8;
+				MainCharObj1[0]->Position.y += 1.0f;
+				data->Index -= 1;
+			}
+		}
+
+		if (MainCharObj1[1]) {
+			if (Fans_IsSpecificPlayerInCylinder(MainCharObj1[1], &data->Position, 130.0f, data->Scale.x)) {
+				data->field_2 = 20;
+			}
+			else {
+				data->field_2 = 0;
+			}
+
+			if (data->field_2 != 0) {
+				MainCharObj2[1]->Speed.y = 0.25f;
+				MainCharObj1[1]->Status |= 0x1000;
+				MainCharObj1[1]->NextAction = 8;
+				MainCharObj1[1]->Position.y += 1.0f;
+				data->field_2 -= 1;
+			}
+		}
+
+		data->Scale.z -= data->Scale.x * 80.0f;
+	}
+}
+
+void __cdecl EFBigFan(ObjectMaster* obj) {
+	EntityData1* data = obj->Data1.Entity;
+
+	if (data->Scale.y != 1) {
+		obj->MainSub = ObjFan;
+		data->Scale.y = data->Scale.x * 80.0f;
+		return;
+	}
+
+	obj->DeleteSub = ObjectFunc_DynColDelete;
+	obj->MainSub = EFBigFan_Main;
+	obj->DisplaySub = EFBigFan_Display;
+	obj->field_1C = EFBigFan_DisplayAlpha;
+
+	Play3DSound_EntityPosBank(obj->Data1.Entity, 7, &obj->Data1.Entity->Position, 50, 5);
+
+	NJS_OBJECT* dynobj = GetFreeDynObject();
+
+	memcpy(dynobj, EF_EBIGFANCOL->getmodel(), sizeof(NJS_OBJECT));
+
+	dynobj->pos[0] = data->Position.x;
+	dynobj->pos[1] = data->Position.y;
+	dynobj->pos[2] = data->Position.z;
+	dynobj->ang[1] = data->Rotation.y;
+	dynobj->evalflags = 0xFFFFFFF8;
+
+	DynCol_Add(0x1, obj, dynobj);
+
+	obj->EntityData2 = (UnknownData2*)dynobj;
+	obj->field_4C = EF_EBIGFAN->getmodel();
 }
 
 void __cdecl EFCannon(ObjectMaster* obj) {
@@ -72,8 +174,10 @@ void __cdecl EFHelice_Main(ObjectMaster* obj) {
 void __cdecl EFHelice(ObjectMaster* obj) {
 	obj->MainSub = EFHelice_Main;
 	obj->DisplaySub = EFHelice_Display;
+	
+	Play3DSound_EntityPosBank(obj->Data1.Entity, 7, &obj->Data1.Entity->Position, 70, 5);
+
 	obj->field_4C = EF_EHELICE->getmodel();
-	Play3DSound_EntityPosBank(obj->Data1.Entity, 8, &obj->Data1.Entity->Position, 100, 5);
 }
 
 void __cdecl ECBarrier(ObjectMaster* obj) {
@@ -127,7 +231,7 @@ ObjectListEntry EggFleetObjectList_list[] = {
 	{ LoadObj_Data1, ObjIndex_Stage, DistObj_UseDist, 360000, nullptr },
 	{ LoadObj_Data1, ObjIndex_Stage, DistObj_UseDist, 1560000, OBJCASE },
 	{ LoadObj_Data1, ObjIndex_Stage, DistObj_UseDist, 1360000, (ObjectFuncPtr)UDREEL }, //45
-	{ LoadObj_Data1, ObjIndex_Stage, DistObj_UseDist, 4060000, EFBigFans },
+	{ LoadObj_Data1, ObjIndex_Stage, DistObj_UseDist, 4060000, EFBigFan },
 	{ LoadObj_Data1, ObjIndex_Stage, DistObj_UseDist, 1060000, EFCannon },
 	{ LoadObj_Data1, ObjIndex_Stage, DistObj_UseDist, 3060000, EFPlatforms },
 	{ LoadObj_Data1, ObjIndex_Stage, DistObj_UseDist, 3060000, EFPipeline },
@@ -178,12 +282,15 @@ void EggFleet_Load() {
 	LoadLevelMusic((char*)"eggfleet.adx");
 	LoadDeathZones(EggFleetDeathZones);
 	LoadFogData_Fogtask("stg13_fog.bin", (FogData*)0x1A280C8);
+	LoadStageSounds("se_ac_gf.mlt", (void*)0x8A0F60);
 	
 	LoadObject((LoadObj)0, "SKYBOX", EggFleet_SkyBox, 1)->DisplaySub = EggFleet_SkyBox;
 
 	EF_SKYMDLS = LoadMDL("EF_SKYMDLS", ModelFormat_SA2B);
 	EF_PROPPLR = LoadMDL("EF_PROPPLR", ModelFormat_SA2B);
 	EF_EHELICE = LoadMDL("EF_EHELICE", ModelFormat_SA2B);
+	EF_EBIGFAN = LoadMDL("EF_EBIGFAN", ModelFormat_SA2B);
+	EF_EBIGFANCOL = LoadMDL("EF_EBIGFAN", ModelFormat_Basic);
 
 	SetPropellerModel(EF_PROPPLR->getmodel());
 }
@@ -192,6 +299,8 @@ void EggFleetDelete() {
 	FreeMDL(EF_SKYMDLS);
 	FreeMDL(EF_PROPPLR);
 	FreeMDL(EF_EHELICE);
+	FreeMDL(EF_EBIGFAN);
+	FreeMDL(EF_EBIGFANCOL);
 
 	CommonLevelDelete();
 }
